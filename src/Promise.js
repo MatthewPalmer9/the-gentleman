66da52fedd73e528337317f473df03b26c94e7f2
MacBookPro <macbookpro@MacBookPros-MBP.attlocal.net>
2020-06-18 12:25:32 -0500

    Fixing a spelling error and a mistake style on svg elements.

--- src/Promise.js
+++ src/Promise.js
@@ -53,7 +53,7 @@ export default class Promise extends Component {
                                 <path d="M22 11.08V12a10 10 0 11-5.93-9.14"></path>
                                 <path d="M22 4L12 14.01l-3-3"></path>
                             </svg>
-                            <span className="title-font font-medium">Afordable Financing</span>
+                            <span className="title-font font-medium">Affordable Financing</span>
                             </div>
                         </div>
                         <div className="p-2 sm:w-1/2 w-full">
